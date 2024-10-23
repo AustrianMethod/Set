da4dd70b7bdb1fddbaef6f8aa8ba5e9e7f109648
AustrianMethod <ivanov.nv.spb@gmail.com>
2024-10-23 17:31:26 +0300

    done

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,7 +1,32 @@
-import sum from '../basic';
+import Team, {Character} from '../app';
 
-test('should sum', () => {
-  const result = sum([1, 2, 3]);
+test('add func', () => {
+  const team = new Team;
+  const [char1, char2] = [new Character('Mike'), new Character('Mike')];
+  team.add(char1, char2);
 
-  expect(result).toBe(6);
+  const result1 = team.members.has('Mike');
+  const result2 = team.members.size;
+  expect(result1).toBe(true);
+  expect(result2).toBe(1);
+});
+
+test('addAll func', () => {
+  const team = new Team;
+  const [char1, char2, char3] = [new Character('Mike'), new Character('Bo'), new Character('Mike')];
+  team.addAll(char1, char2, char3);
+
+  const result1 = team.members.has('Mike');
+  const result2 = team.members.size;
+  expect(result1).toBe(true);
+  expect(result2).toBe(2);
+});
+
+test('toArray func', () => {
+  const team = new Team;
+  const [char1, char2, char3] = [new Character('Mike'), new Character('Bo'), new Character('Mike')];
+  team.addAll(char1, char2, char3);
+
+  const result = team.toArray();
+  expect(result).toEqual(['Mike', 'Bo']);
 });
--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,25 @@
-// TODO: write your code here
-import sum from './basic';
-
-console.log('worked');
-
-console.log(sum([1, 2]));
+export default class Team {
+    constructor() {
+      this.members = new Set();
+    }
+  
+    add(obj) {
+      this.members.add(obj.name);
+    }
+  
+    addAll(...arr) {
+      arr.forEach( (obj) => {
+        this.members.add(obj.name)
+      });
+    }
+  
+    toArray() {
+      return Array.from(this.members);
+    }
+  }
+  
+export class Character {
+    constructor(name) {
+      this.name = name;
+    }
+  }
